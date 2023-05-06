45bab00f9c34f45586b2eb7f791ba90dc6f322a3
Karolina Sniezek <karolinasniezek@gmail.com>
2023-05-06 16:42:49 +0200

    add models

---
+++ Boards/Boards/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace Boards.Models
+{
+    public class Comment
+    {
+        public string Message { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ Boards/Boards/Models/WorkItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Boards.Models
+{
+    public class WorkItem
+    {
+        public string State { get; set; }
+        public string Area { get; set; }
+        public string IterationPath { get; set; }
+        public int Priority { get; set; }
+
+        // Epic
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        // Issue
+        public decimal Efford { get; set; }
+
+        //Task
+        public string Activity { get; set; }
+        public decimal RemainingWork { get; set; }
+
+        public string Type { get; set; }
+    }
+}
