37b887e6b1b724c3d13a5cd64365698daf752f37
Karolina Sniezek <karolinasniezek@gmail.com>
2023-05-06 17:15:26 +0200

    add dbContext

---
+++ Boards/Boards/Context/BoardsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Boards.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Boards.Context
+{
+    public class BoardsDbContext : DbContext
+    {
+        public BoardsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        public BoardsDbContext(DbContextOptions<BoardsDbContext> options) : base(options)
+        {
+        }
+    }
+}
