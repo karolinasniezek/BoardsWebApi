149c745557ffbfe62606041aa7849710092274f5
Karolina Sniezek <karolinasniezek@gmail.com>
2023-05-06 18:18:52 +0200

    add keys and default values for workItem and comment

--- Boards/Boards/Models/WorkItem.cs
+++ Boards/Boards/Models/WorkItem.cs
@@ -1,19 +1,20 @@
-﻿namespace Boards.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Boards.Models
 {
     public class WorkItem
     {
+        public int Id { get; set; }
         public string State { get; set; }
         public string Area { get; set; }
         public string IterationPath { get; set; }
         public int Priority { get; set; }
-
         // Epic
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-
         // Issue
         public decimal Efford { get; set; }
-
         //Task
         public string Activity { get; set; }
         public decimal RemainingWork { get; set; }
